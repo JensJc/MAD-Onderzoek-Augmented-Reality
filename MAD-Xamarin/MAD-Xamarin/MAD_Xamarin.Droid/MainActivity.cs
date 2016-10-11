a120acf9d01a531c4a86beb96c45cb85163d4057
Jens Cobussen <j.cobussen@infostrait.nl>
2016-10-11 10:47:28 +0200

    Initial commit #1

---
+++ MAD-Xamarin/MAD-Xamarin/MAD_Xamarin.Droid/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace MAD_Xamarin.Droid
+{
+	[Activity (Label = "MAD_Xamarin", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+			LoadApplication (new MAD_Xamarin.App ());
+		}
+	}
+}
+
