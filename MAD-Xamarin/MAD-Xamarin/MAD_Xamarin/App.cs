71e082706a20394c01ff00f0cfe56e9c6e7b77e9
Matthieudewit <m_le_blanc@hotmail.com>
2016-10-27 18:08:43 +0200

    MainPage changed
    Added MyMainPage to project

--- MAD-Xamarin/MAD-Xamarin/MAD_Xamarin/App.cs
+++ MAD-Xamarin/MAD-Xamarin/MAD_Xamarin/App.cs
@@ -12,17 +12,19 @@ namespace MAD_Xamarin
 		public App ()
 		{
 			// The root page of your application
-			MainPage = new ContentPage {
-				Content = new StackLayout {
-					VerticalOptions = LayoutOptions.Center,
-					Children = {
-						new Label {
-							HorizontalTextAlignment = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
-						}
-					}
-				}
-			};
+			//MainPage = new ContentPage {
+			//	Content = new StackLayout {
+			//		VerticalOptions = LayoutOptions.Center,
+			//		Children = {
+			//			new Label {
+			//				HorizontalTextAlignment = TextAlignment.Center,
+			//				Text = "Welcome to Xamarin Forms!?"
+			//			}
+			//		}
+			//	}
+			//};
+
+			MainPage = new MyMainPage();
 		}
 
 		protected override void OnStart ()
