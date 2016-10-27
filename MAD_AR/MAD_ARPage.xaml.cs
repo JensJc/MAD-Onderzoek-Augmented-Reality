35484d59a440c1cb4cbfc1f35b98728abc62b790
Matthieudewit <m_le_blanc@hotmail.com>
2016-10-27 16:40:30 +0200

    New Xamarin MAD-AR solutionsolution

---
+++ MAD_AR/MAD_ARPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace MAD_AR
+{
+	public partial class MAD_ARPage : ContentPage
+	{
+		public MAD_ARPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ MAD_AR/MyMainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MAD_AR
+{
+	public partial class MyMainPage : ContentPage
+	{
+		public MyMainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
